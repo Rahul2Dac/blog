b982eb2cd8e5b6c4ece9cf0f4381ff86a59e7181
Rahul Pr <rahuldac4141@gmail.com>
2020-09-02 09:10:19 -0700

    blog

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'Rahul',
+        'title'  : 'Blog Post 1',
+        'content': 'First post content',
+        'date posted': 'August 27, 2020'
+
+    },
+    {
+        'author' : 'Akkz',
+        'title'  : 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020'
+
+    }
+
+]
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request,'blog/home.html',context)
+
+
+def about(request):
+     return render(request,'blog/about.html')
