2e20e8025c36a2200526cd5568cbbe6a22270b86
Rahul Pr <rahuldac4141@gmail.com>
2020-09-04 10:48:07 -0700

    custom user update

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'users',
     'user',
+    'users',
+    
     'crispy_forms',
 ]
 
@@ -136,4 +137,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'user.CustomUser'
